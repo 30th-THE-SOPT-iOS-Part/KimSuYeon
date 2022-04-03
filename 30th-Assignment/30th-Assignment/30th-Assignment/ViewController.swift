b51b07302bcfa074a43ecd980e27262ca5efef59
suyeon <rlatndus9911@naver.com>
2022-04-04 02:56:28 +0900

    [ADD]  과제 프로젝트 생성 (#1)

---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
