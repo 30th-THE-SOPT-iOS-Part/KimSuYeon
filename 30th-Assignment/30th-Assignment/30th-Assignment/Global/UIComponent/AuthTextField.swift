466f49c5b083957edf71a08aee153ab3dbae78c1
suyeon <rlatndus9911@naver.com>
2022-04-05 03:41:09 +0900

    [FEAT] 커스텀 텍스트필드 추가 (#2)

---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Global/UIComponent/AuthTextField.swift
@@ -0,0 +1,42 @@
+//
+//  AuthTextField.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/04/04.
+//
+
+import UIKit
+
+import SnapKit
+
+class AuthTextField: UITextField {
+
+    private let leftSpaceView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 50))
+
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        setUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setUI() {
+        leftView = leftSpaceView
+        leftViewMode = .always
+
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.cornerRadius = 5
+        
+        textColor = .black
+        backgroundColor = .white
+        attributedPlaceholder = NSAttributedString(string: placeholder ?? "" , attributes: [.foregroundColor: UIColor.darkGray])
+
+        self.snp.makeConstraints {
+            $0.height.equalTo(50)
+        }
+    }
+}
