f94b904e761c045d0509c2a897cfa03164da9e87
suyeon <rlatndus9911@naver.com>
2022-05-17 01:29:20 +0900

    [FEAT] AuthRouter 구현

--- 30th-Assignment/30th-Assignment/30th-Assignment/Network/Router/AuthRouter.swift
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Network/Router/AuthRouter.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import Alamofire
+
+enum AuthRouter {
+    case signIn(email: String, pw: String)
+    case signUp(email: String, name: String, pw: String)
+}
+
+extension AuthRouter: Router {
+
+    var path: String {
+        switch self {
+        case .signIn:
+            return URLConstants.signIn
+        case .signUp:
+            return URLConstants.signUp
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .signIn, .signUp:
+            return .post
+        }
+    }
+
+    var header: HeaderType {
+        switch self {
+        case .signIn, .signUp:
+            return .basic
+        }
+    }
+
+    var parameters: RequestParams {
+        switch self {
+        case .signUp(let email, let name, let pw):
+            let body: [String:Any] = [
+                "email": email,
+                "name": name,
+                "password":pw
+            ]
+            return .requestParameters(body)
+        case .signIn(let email, let pw):
+            let body: [String:Any] = [
+                "email": email,
+                "password": pw
+            ]
+            return .requestParameters(body)
+        }
+    }
+}
