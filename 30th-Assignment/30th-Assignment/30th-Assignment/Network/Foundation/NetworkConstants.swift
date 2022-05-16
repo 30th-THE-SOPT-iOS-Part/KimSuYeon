a4ac9631bda25e08ce74b6a1819cdb8433afc67b
suyeon <rlatndus9911@naver.com>
2022-05-16 23:27:47 +0900

    [FEAT] Constants 추가

---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Network/Foundation/NetworkConstants.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkConstants.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/05/16.
+//
+
+import Foundation
+
+struct NetworkConstants {
+
+    static let header = ["Content-Type": "application/json"]
+}
---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Network/Foundation/URLConstants.swift
@@ -0,0 +1,14 @@
+//
+//  URLConstants.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/05/16.
+//
+
+import Foundation
+
+struct URLConstants {
+    
+    static let authSignup = "/auth/signup"
+    static let authSignin = "/auth/signin"
+}
