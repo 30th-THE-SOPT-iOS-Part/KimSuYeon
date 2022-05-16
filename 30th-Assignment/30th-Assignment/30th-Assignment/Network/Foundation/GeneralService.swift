6ff289656d041b1572071f11fe6ed458b34be4be
suyeon <rlatndus9911@naver.com>
2022-05-16 23:31:00 +0900

    [FEAT] GeneralService 구현

---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Network/Foundation/GeneralService.swift
@@ -0,0 +1,30 @@
+//
+//  GeneralResponse.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/05/16.
+//
+
+import Foundation
+
+struct GeneralService {
+
+    static func judgeStatus<T: Codable>(by statusCode: Int, _ data: Data, _ type: T.Type) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GeneralResponse<T>.self, from: data)
+        else { return .pathErr }
+        print(decodedData)
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "None-Data")
+        case 201..<300:
+            return .success(decodedData.status)
+        case 400..<500:
+            return .requestErr(decodedData.status)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
