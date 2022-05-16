7e9468f036ee4fd639826d40c93d4fe8c41ac97d
suyeon <rlatndus9911@naver.com>
2022-05-16 23:26:43 +0900

    [FEAT] APIEnvironment 추가

---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Network/Foundation/APIEnvironment.swift
@@ -0,0 +1,24 @@
+//
+//  APIEnvironment.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/05/16.
+//
+
+import Foundation
+
+enum APIEnvironment: String, CaseIterable {
+    case development
+    case production
+}
+
+extension APIEnvironment {
+    var baseUrl: String {
+        switch self {
+        case .development:
+            return "http://13.124.62.236/"
+        case .production:
+            return ""
+        }
+    }
+}
