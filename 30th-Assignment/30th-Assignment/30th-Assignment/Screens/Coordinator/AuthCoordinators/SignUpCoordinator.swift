5c69b2ae55f3941b45e25c4eb20751db382d61f1
suyeon <rlatndus9911@naver.com>
2022-05-19 11:10:45 +0900

    [ADD] 코디네이터 전체 폴더 구조 세팅

---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Screens/Coordinator/AuthCoordinators/SignUpCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpCoordinator.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/05/19.
+//
+
+import Foundation
---
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Screens/Coordinator/Foundation/TransitionModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransitionModel.swift
+//  30th-Assignment
+//
+//  Created by 김수연 on 2022/05/19.
+//
+
+import Foundation
