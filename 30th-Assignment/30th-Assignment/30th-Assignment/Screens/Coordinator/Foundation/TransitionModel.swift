7bf6e68de4c5686420dcb29fb971691d540ac52e
suyeon <rlatndus9911@naver.com>
2022-05-21 09:54:13 +0900

    [FEAT] Transition Model 추가

--- 30th-Assignment/30th-Assignment/30th-Assignment/Screens/Coordinator/Foundation/TransitionModel.swift
+++ 30th-Assignment/30th-Assignment/30th-Assignment/Screens/Coordinator/Foundation/TransitionModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+/// 화면전환 스타일 enum
+
+enum TransitionStyle {
+    case root
+    case push
+    case modal
+}
+
+enum TransitionError {
+    case navigationControllerMissing
+    case cannotPop
+    case unknown
+}
